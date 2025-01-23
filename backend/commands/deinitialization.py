2df7f98fc7d92e8ad695c61d82b4e7bfbc2112ac
raiz <128097369+razingash@users.noreply.github.com>
2025-01-23 17:46:18 +0100

    v0.2.3b1
    fixed bug with incorrect exception handling in the deinitialization command, caused by the __pycache__ directory

--- backend/commands/deinitialization.py
+++ backend/commands/deinitialization.py
@@ -1,5 +1,8 @@
 import os
+import shutil
 from glob import glob
+from pathlib import Path
+
 from colorama import Style, Fore
 
 from core.config import BASE_DIR
@@ -12,9 +15,13 @@ def command_deinitialization():
             migrations_dir = os.path.join(BASE_DIR, 'alembic', 'versions')
             os.remove('db.sqlite3')
             migrations = glob(os.path.join(migrations_dir, "*"))
-            [os.remove(migration) for migration in migrations]
+            for migration in migrations:
+                if Path(migration).is_file():
+                    os.remove(migration)
+                elif Path(migration).is_dir():
+                    shutil.rmtree(migration)
         except PermissionError:
-            print(Fore.LIGHTRED_EX + "the file is occupied by another process, most likely the database is open in some application")
+            print(Fore.LIGHTRED_EX + "the file is occupied by another process, most likely the database is open in some other application")
         else:
             print(Style.BRIGHT + Fore.GREEN + "deinitialization completed")
     else:
